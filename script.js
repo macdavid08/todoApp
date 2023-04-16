7a8ad4016feec406e21e1b6e7a5f28dc4c2938dd
Macford <99491939+macdavid08@users.noreply.github.com>
2023-04-16 21:05:47 +0800

    todo

---
+++ script.js
@@ -0,0 +1,31 @@
+const todoApp = {
+    data() {
+        return {
+            todos:[],
+            newTodo: {
+                done: false
+            }
+        }
+    },
+    methods: {
+        addTo: function () {
+            if(this.newTodo.text){
+
+                this.todos.push(this.newTodo);
+                this.newTodo = {
+                    done: false
+                },
+                localStorage.setItem("todos", JSON.stringify(this.todos))
+            }
+        }
+    },
+
+    created() {
+        this.todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : this.todos;
+    },
+    updated() {
+        localStorage.setItem("todos", JSON.stringify(this.todos))
+    }
+}
+
+Vue.createApp(todoApp).mount("#app")
